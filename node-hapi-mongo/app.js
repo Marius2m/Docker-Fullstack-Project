38a313ab8bb3510c2c36c3ed8c078280a5857459
Marius Mircea <mariusmircea@outlook.com>
2019-06-22 22:19:41 +0300

    Mongoose & MongoDB Init
    + set up Mongoose connection to MongoDB
    + implemented first GET endpoint used to determine the status of the connection to MongoDB

--- node-hapi-mongo/app.js
+++ node-hapi-mongo/app.js
@@ -1,4 +1,9 @@
 const Hapi = require("@hapi/hapi");
+const mongoose = require("mongoose");
+const connectionString = 'mongodb+srv://Marius2m:parola@haufe-y2a8i.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(
+  connectionString
+);
 
 const init = async () => {
 
@@ -19,6 +24,16 @@ const init = async () => {
       path: '/test',
       handler: (request, h) => {
         return new Promise((resolve, reject) => resolve(h.response('amazing!').code(200)));
+      }
+    });
+
+    // 1ST ENDPOINT
+    server.route({
+      method: 'GET',
+      path: '/mongoStatus',
+      handler: (request, h) => {
+        const mongooseState = mongoose.STATES[mongoose.connection.readyState];
+        return { "mongo status" : mongooseState };
       }
     });
 
